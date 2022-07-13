deeb0247c83a3cfb2b2afdaa1b4f6f8cb8d57e7d
Adam Azad <adam@adamazad.com>
2022-07-13 11:47:14 +0100

    feat: add Fantom tracking

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,35 @@
 import type { AppProps } from 'next/app'
-import Head from 'next/head'
+import { useRouter } from 'next/router'
+import * as Fathom from 'fathom-client'
+import { useEffect } from 'react'
+
 import { FixedGlobalStyle, ThemeProvider } from '../theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  useEffect(() => {
+    if (!process.env.FATHOM_TRACKING_CODE) {
+      return
+    }
+
+    Fathom.load(process.env.FATHOM_TRACKING_CODE, {
+      url: window.location.href,
+    })
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview()
+    }
+
+    // Record a pageview when route changes
+    router.events.on('routeChangeComplete', onRouteChangeComplete)
+
+    // Unassign event listener
+    return () => {
+      router.events.off('routeChangeComplete', onRouteChangeComplete)
+    }
+  })
+
   return (
     <ThemeProvider>
       <FixedGlobalStyle />
