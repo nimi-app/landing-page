396e23e340a90756d0add3b9f36cbb8e36238f4e
Adam Azad <adam@adamazad.com>
2022-12-06 12:44:03 +0000

    chore(docker): update build.sh

--- docker/build.sh
+++ docker/build.sh
@@ -2,21 +2,33 @@
 
 # Extract build information from git
 GIT_COMMIT=$(git rev-parse HEAD)
-GIT_TAG_NAME=$(git tag --points-at HEAD)
-GIT_BRANCH_NAME=$(git branch --show-current)
+GIT_TAG=$(git tag --points-at HEAD)
+GIT_BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD | sed 's/[^a-zA-Z0-9]/-/g')
+
+# Initial docker image info
+DOCKER_IMAGE_NAME="nimi-landing-page"
+DOCKER_IMAGE_VERSION=$(echo $GIT_TAG | sed 's/v//g')
 
 # Use tag if exists, resort to branch name
 GIT_REVISION=$GIT_TAG
 
-if [ "${GIT_TAG}" == "" ]; then
+if [ !$GIT_TAG ]
+then
     GIT_REVISION=$GIT_BRANCH_NAME
+    DOCKER_IMAGE_VERSION=$GIT_BRANCH_NAME
+elif [ !$GIT_BRANCH_NAME ]
+then
+    GIT_REVISION=$GIT_COMMIT
+    DOCKER_IMAGE_VERSION=$GIT_COMMIT
 fi
 
 # Construct Docker image
-# DOCKER_TAG=$GIT_REVISION
-DOCKER_IMAGE_NAME="nimi-landing-page"
-DOCKER_IMAGE="${DOCKER_IMAGE_NAME}"
+# Final image name is in the form of <image_name>:<image_version>
+DOCKER_IMAGE_TAG="${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_VERSION}"
+# Build Docker image
+echo "Building ${DOCKER_IMAGE_TAG} @ ${GIT_COMMIT} from ${GIT_REVISION}"
 
-echo "Building ${DOCKER_IMAGE} @ ${GIT_COMMIT}"
-
-docker build --file ./docker/Dockerfile -t ${DOCKER_IMAGE} --build-arg "GIT_COMMIT=${GIT_COMMIT}" --build-arg "GIT_REVISION=${GIT_REVISION}" .
+docker build --file ./docker/Dockerfile \
+--build-arg "GIT_COMMIT=${GIT_COMMIT}" \
+--build-arg "GIT_REVISION=${GIT_REVISION}" \
+--tag ${DOCKER_IMAGE_TAG} .
