c0e12b82e58964fc26ce2426ce52c724f9fe4f13
Adam Azad <adam@adamazad.com>
2023-03-28 14:11:31 +0100

    chore(ci): add private repo support to docker

--- docker/build.sh
+++ docker/build.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# Check for SSH_PRIVATE_KEY and SSH_PUBLIC_KEY environment variables
+if [ -z "$SSH_PRIVATE_KEY" ]; then
+    echo "SSH_PRIVATE_KEY environment variable not set. This is required to clone @nimi.io/card npm package."
+    exit 1
+fi
+
+if [ -z "$SSH_PUBLIC_KEY" ]; then
+    echo "SSH_PUBLIC_KEY environment variable not set. This is required to clone @nimi.io/card npm package."
+    exit 1
+fi
+
 # Extract build information from git
 GIT_COMMIT=$(git rev-parse HEAD)
 GIT_TAG=$(git tag --points-at HEAD)
@@ -29,6 +40,8 @@ DOCKER_IMAGE_TAG="${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_VERSION}"
 echo "Building ${DOCKER_IMAGE_TAG} @ ${GIT_COMMIT} from ${GIT_REVISION}"
 
 docker build --file ./docker/Dockerfile \
+--build-arg SSH_PRIVATE_KEY="$SSH_PRIVATE_KEY" \
+--build-arg SSH_PUBLIC_KEY="$SSH_PUBLIC_KEY" \
 --build-arg "GIT_COMMIT=${GIT_COMMIT}" \
 --build-arg "GIT_REVISION=${GIT_REVISION}" \
 --tag ${DOCKER_IMAGE_TAG} .
